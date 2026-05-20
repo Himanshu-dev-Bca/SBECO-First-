41b59bfc2e71fa3b6bd8f2441644a76107fa2a2d
div-html <pariyardivyam67@gmail.com>
2026-05-20 11:51:43 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from '@vercel/analytics/react';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -42,8 +41,8 @@ export default function App() {
         <Route path="/products/:productId" element={<Layout hideFooter><ProductDetail /></Layout>} />
         <Route path="/photos" element={<Layout><Photos /></Layout>} />
       </Routes>
-      <SpeedInsights />
-      <Analytics />
+      
+     
     </BrowserRouter>
   );
 }
